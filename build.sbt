3667a6d94f09e4f1a5f2fdbdaf96f72ff383c13f
Karl Higley <kmhigley@gmail.com>
2015-04-01 10:57:40 -0400

    Rank sentences from a single document by informativity using PageRank
    This uses sentence segmentation and tokenization from Chalk, a ScalaNLP
    library.  Both are rule-based and have some shortcomings, but they're
    a decent starting point.
    
    Tokenized sentences are converted to vectors using Spark's TFIDF
    featurization utility and used as vertices in a graph.  Sentences with
    cosine similarity greater than 0.1 are connected by edges.  Running
    PageRank on the graph produces sentence scores that indicate how
    informative they are relative to other sentences in the document.
    
    (Edges in the graph are weighted to make it easy to filter/prune them based on
    cosine similarity, but Spark's PageRank implementation ignores the weights, so
    they are effectively unweighted when computing sentence scores.)

---
+++ build.sbt
@@ -0,0 +1,18 @@
+name := "lexrank-summarizer"
+
+version := "0.0.1"
+
+scalaVersion := "2.10.4"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "1.3.0" % "provided",
+  "org.apache.spark" %% "spark-mllib" % "1.3.0" % "provided",
+  "org.scalanlp" %% "breeze" % "0.11.2",
+  "org.scalanlp" %% "chalk" % "1.3.2" exclude ("com.typesafe.sbt", "sbt-pgp")
+)
+
+resolvers ++= Seq(
+  "Akka Repository" at "http://repo.akka.io/releases/",
+  "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
+  "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/"
+)
---
+++ src/main/scala/io/github/karlhigley/summarizer.scala
@@ -0,0 +1,81 @@
+package io.github.karlhigley
+
+import org.apache.spark.{SparkContext, SparkConf, Logging}
+import org.apache.spark.rdd.RDD
+
+import org.apache.spark.mllib.feature.{HashingTF, IDF, Normalizer}
+import org.apache.spark.mllib.linalg.{Vector}
+
+import org.apache.spark.graphx._
+
+import breeze.linalg.{DenseVector => BDV}
+
+import chalk.text.segment.JavaSentenceSegmenter
+import chalk.text.tokenize.SimpleEnglishTokenizer
+
+case class Sentence(text: String, features: Vector)
+
+object Summarizer extends Logging {
+  type Vertex = Tuple2[Long, Sentence]
+
+  val hashingTF = new HashingTF()
+
+  def dot(v1: Vector, v2: Vector) : Double = {
+    BDV(v1.toArray).dot(BDV(v2.toArray))
+  }
+
+  def tokenize(text: String) : Seq[String] = {
+    val tokenizer = SimpleEnglishTokenizer()
+    tokenizer(text.trim.toLowerCase).toSeq    
+  }
+
+  def segment(text: String) : Seq[String] = {
+    JavaSentenceSegmenter(text).toSeq
+  }
+
+  def featurizeSentences(rawSentences: RDD[String]) : RDD[Sentence] = {
+    val sentencesWithTFs = rawSentences.map(s => (s, hashingTF.transform(tokenize(s))))
+    
+    val idf = new IDF().fit(sentencesWithTFs.map(_._2))    
+    val normalizer = new Normalizer()    
+
+    sentencesWithTFs.map( swtf => {      
+      val featureVector = normalizer.transform(idf.transform(swtf._2))
+      new Sentence(swtf._1, featureVector)
+    })
+  }
+
+  def buildEdges(vertices: RDD[Vertex]) : RDD[Edge[Double]] = {
+    vertices
+      .cartesian(vertices)
+      .filter(pair => pair._1._2.text != pair._2._2.text)
+      .distinct()
+      .map( pair => Edge(pair._1._1, pair._2._1, dot(pair._1._2.features, pair._2._2.features)) )
+  }
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Summarizer")
+    val sc   = new SparkContext(conf)
+
+    val rawSentences = sc.textFile("test.txt").flatMap(segment)
+    
+    val vertices = featurizeSentences(rawSentences).zipWithIndex().map(v => (v._2, v._1))
+    val edges    = buildEdges(vertices)
+
+    val sentenceGraph: Graph[Sentence, Double] = Graph(vertices, edges)
+
+    val ranks = sentenceGraph
+                  .subgraph(epred = (edge) => edge.attr >= 0.1)
+                  .pageRank(0.0001)
+                  .vertices
+
+    val sentencesByRank = vertices
+                            .join(ranks)
+                            .map { case (id, (sentence, rank)) => (rank, sentence.text) }
+                            .sortByKey(false)
+
+    sentencesByRank.saveAsTextFile("ranked-sentences")
+
+    sc.stop()
+  }
+}
