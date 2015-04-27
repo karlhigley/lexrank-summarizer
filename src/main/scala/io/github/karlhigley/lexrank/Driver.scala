145679dedd5d2b06468d5c16f164572e07609be5
Karl Higley <kmhigley@gmail.com>
2015-04-27 15:31:58 -0400

    Move featurization to separate class and excerpt selection to Driver

--- src/main/scala/io/github/karlhigley/lexrank/Driver.scala
+++ src/main/scala/io/github/karlhigley/lexrank/Driver.scala
@@ -3,8 +3,19 @@ package io.github.karlhigley.lexrank
 import scala.io.Source
 
 import org.apache.spark.{SparkContext, SparkConf, Logging}
+import org.apache.spark.rdd.RDD
+
+import org.apache.spark.graphx._
 
 object Driver extends Logging {
+  private def selectExcerpts(sentences: RDD[Sentence], scores: VertexRDD[Double], length: Int) = {
+    scores
+      .join(sentences.map(s => (s.id, s)))
+      .map { case (_, (score, sentence)) => (sentence.docId, (score, sentence.id, sentence.text)) }
+      .groupByKey()
+      .flatMap { case (docId, sentences) => sentences.toSeq.sortWith(_._1 > _._1).take(length).map(e => (docId, e._3)) }
+  }
+
   def main(args: Array[String]) {
     val sparkConf = new SparkConf().setAppName("Summarizer")
     val sc        = new SparkContext(sparkConf)
@@ -13,6 +24,7 @@ object Driver extends Logging {
 
     val stopwords = Source.fromFile(config.stopwordsPath).getLines.toSet
     sc.broadcast(stopwords)
+    val featurizer = new Featurizer(stopwords)
 
     val documents = sc.textFile(config.inputPath).flatMap( 
       _.split('\t').toList match {
@@ -21,10 +33,13 @@ object Driver extends Logging {
       }
     )
 
-    val model    = LexRank.featurize(documents, stopwords)
-    val excerpts = model.summarize(config.length, config.threshold, config.cutoff, config.convergence)
+    val (sentences, features) = featurizer.featurize(documents)
 
-  excerpts
+    val model    = new LexRank(features)
+    val ranks    = model.score(config.threshold, config.cutoff, config.convergence)
+    val excerpts = selectExcerpts(sentences, ranks, config.length)
+
+    excerpts
       .map(_.productIterator.toList.mkString("\t"))
       .saveAsTextFile(config.outputPath)
 
---
+++ src/main/scala/io/github/karlhigley/lexrank/Featurizer.scala
@@ -0,0 +1,68 @@
+package io.github.karlhigley.lexrank
+
+import org.apache.spark.rdd.RDD
+
+import org.apache.spark.mllib.feature.{HashingTF, IDF}
+import org.apache.spark.mllib.linalg.SparseVector
+
+import chalk.text.analyze.PorterStemmer
+import chalk.text.segment.JavaSentenceSegmenter
+import chalk.text.tokenize.SimpleEnglishTokenizer
+
+case class Document(id: String, text: String)
+case class Sentence(id: Long, docId: String, text: String)
+case class SentenceTokens(id: Long, docId: String, tokens: Seq[String])
+case class SentenceFeatures(id: Long, docId: String, features: SparseVector)
+
+class Featurizer(stopwords: Set[String]) extends Serializable {
+  def featurize(documents: RDD[Document]) = {  
+    val sentences = extractSentences(documents)
+    val tokenized = tokenize(sentences, stopwords)
+    val features  = vectorize(tokenized)
+    (sentences, features)
+  }
+
+  private def extractSentences(documents: RDD[Document]) : RDD[Sentence] = {
+    documents
+      .flatMap(d => segment(d.text).map(t => (d.id, t)) )
+      .zipWithIndex()
+      .map({
+        case ((docId, sentenceText), sentenceId) => Sentence(sentenceId, docId, sentenceText)
+      })
+  }
+
+  private def tokenize(sentences: RDD[Sentence], stopwords: Set[String]) : RDD[SentenceTokens] = {
+    val tokenizer = SimpleEnglishTokenizer()
+    sentences.map(s => {
+      val tokens = tokenizer(s.text.toLowerCase).toSeq.filter(!stopwords.contains(_)).map(stem)
+      SentenceTokens(s.id, s.docId, tokens)
+    })
+  }
+
+  private def vectorize(tokens: RDD[SentenceTokens]) : RDD[SentenceFeatures] = {
+    val hashingTF  = new HashingTF()
+    val idfModel   = new IDF()
+
+    val termFrequencies = tokens.map(t => {
+        (t.id, t.docId, hashingTF.transform(t.tokens))
+    })
+    
+    val idf = idfModel.fit(termFrequencies.map({ case (_, _, tf) => tf }))
+
+    termFrequencies.map({
+      case (id, docId, tf) =>
+        val featureVector = idf.transform(tf).asInstanceOf[SparseVector]
+        SentenceFeatures(id, docId, featureVector)
+    })
+  }
+
+  private def segment(text: String) : Seq[String] = {
+    JavaSentenceSegmenter(text).toSeq
+  }
+
+  private def stem(token: String) : String = {
+    PorterStemmer(token)
+  }
+}
+
+  
