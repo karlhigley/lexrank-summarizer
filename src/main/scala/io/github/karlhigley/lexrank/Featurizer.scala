d2b4e72449282dfe0bb2b073934ff3189d30ea55
Karl Higley <kmhigley@gmail.com>
2015-05-12 16:46:53 -0400

    Filter out short tokens (typically punctuation and numbers)
    Given the naive tokenization being used, the most common tokens are "."
    and ",".  The high frequency of these punctuation tokens results in some
    features that are completely uninformative and require a lot of extra
    effort in the similarity computation.  (Since each feature is a matrix
    row and the matrices are partitioned by row, some partitions take a long
    time to compute and add very little to the overall result.)  Filtering
    out these tokens reduces the skew in matrix entries across partitions,
    and therefore reigns in the straggler tasks a bit.

--- src/main/scala/io/github/karlhigley/lexrank/Featurizer.scala
+++ src/main/scala/io/github/karlhigley/lexrank/Featurizer.scala
@@ -34,7 +34,11 @@ class Featurizer(stopwords: Set[String]) extends Serializable {
   private def tokenize(sentences: RDD[Sentence], stopwords: Set[String]) : RDD[SentenceTokens] = {
     val tokenizer = SimpleEnglishTokenizer()
     sentences.map(s => {
-      val tokens = tokenizer(s.text.toLowerCase).toSeq.filter(!stopwords.contains(_)).map(stem)
+      val tokens = tokenizer(s.text.toLowerCase).toSeq
+                                          .filter(_.length > 3)
+                                          .filter(!stopwords.contains(_))
+                                          .map(stem)
+
       SentenceTokens(s.id, s.docId, tokens)
     })
   }
