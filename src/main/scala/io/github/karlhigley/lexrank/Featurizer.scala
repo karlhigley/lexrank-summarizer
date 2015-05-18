ff95daa430a46f0945f7566aa063873fb6490577
Karl Higley <kmhigley@gmail.com>
2015-05-18 15:18:59 -0400

    Revert "Combine input entries with the same identifier into a single document"
    This reverts commit 015326ce81f53172d05bed1545cba2374ada2705.

--- src/main/scala/io/github/karlhigley/lexrank/Featurizer.scala
+++ src/main/scala/io/github/karlhigley/lexrank/Featurizer.scala
@@ -25,7 +25,6 @@ class Featurizer(stopwords: Set[String]) extends Serializable {
   private def extractSentences(documents: RDD[Document]) : RDD[Sentence] = {
     documents
       .flatMap(d => segment(d.text).map(t => (d.id, t)) )
-      .distinct()
       .zipWithIndex()
       .map({
         case ((docId, sentenceText), sentenceId) => Sentence(sentenceId, docId, sentenceText)
