ea622c760dc5df19bb478650ff939f8fb1f8aac0
Karl Higley <kmhigley@gmail.com>
2015-09-30 14:49:22 -0400

    Refactor FeaturizerSuite tests to reduce duplication
    An empty list of tokens or a list of tokens that are all filtered out are
    equivalent and can be covered by the same test. There was also some code
    duplication in setting up the list of sentences that could be removed.
    These changes will make it easier to add a test for the identification
    and removal of stopwords.

--- src/test/scala/io/github/karlhigley/lexrank/FeaturizerSuite.scala
+++ src/test/scala/io/github/karlhigley/lexrank/FeaturizerSuite.scala
@@ -3,14 +3,18 @@ package io.github.karlhigley.lexrank
 import org.scalatest.FunSuite
 
 class FeaturizerSuite extends FunSuite with TestSparkContext {
-  val sentence1 = SentenceTokens(1L, "doc1", List("one", "two", "three"))
-  val sentence2 = SentenceTokens(2L, "doc1", List("three", "four", "five"))
-  val sentence3 = SentenceTokens(3L, "doc1", List("five", "six", "one"))
+  val allSentences = List(
+    SentenceTokens(1L, "doc1", List("one", "two", "three")),
+    SentenceTokens(2L, "doc1", List("three", "four", "five")),
+    SentenceTokens(3L, "doc1", List("five", "six", "one")),
+    SentenceTokens(4L, "doc1", List("alpha", "beta", "gamma")),
+    SentenceTokens(5L, "doc1", List())
+  )
 
   val featurizer = new Featurizer
 
   test("feature vectors include only non-zero entries") {
-    val sentences = sc.parallelize(List(sentence1, sentence2, sentence3))
+    val sentences = sc.parallelize(allSentences)
     val featurized = featurizer(sentences).collect()
     featurized.foreach { f =>
         assert(!f.features.values.contains(0.0))
@@ -18,21 +22,15 @@ class FeaturizerSuite extends FunSuite with TestSparkContext {
   }
 
   test("single occurrence tokens are ignored") {
-    val sentences = sc.parallelize(List(sentence1, sentence2, sentence3))
+    val sentences = sc.parallelize(allSentences)
     val featurized = featurizer(sentences).collect()
     assert(featurized(0).features.indices.size === 2)
   }
 
-  test("zero vectors are omitted") {
-    val sentences = sc.parallelize(List(sentence1))
+  test("empty/zero vectors are omitted") {
+    val sentences = sc.parallelize(allSentences)
     val featurized = featurizer(sentences).collect()
-    assert(featurized.length === 0)
+    assert(featurized.length === 3)
   }
 
-  test("empty vectors are omitted") {
-    val sentence = SentenceTokens(4L, "doc1", List())
-    val sentences = sc.parallelize(List(sentence))
-    val featurized = featurizer(sentences).collect()
-    assert(featurized.length === 0)
-  }
 }
